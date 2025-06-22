b3e3d3f32e259e0bd7b1b38b20bf4132fd538158
Luis Guilherme de Almeida Costa <failho42@gmail.com>
2025-06-22 12:31:45 -0300

    att

--- src/controllers/pagamentoController.js
+++ src/controllers/pagamentoController.js
@@ -2,9 +2,14 @@ exports.index = (req, res) => {
     res.render("formaDePagamento", { path: "A"});
 }
 
-exports.store = (req, res) => {
+exports.store = async (req, res) => {
     try {
+        const response = await api.post('/auth/login/', {
+            email: req.session.email,
+        });
+
         
+
     } catch (error) {
         
     }
