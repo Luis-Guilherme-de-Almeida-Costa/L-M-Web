12b2d100553194060721eb532ae797dc96960ae4
Luis-Guilherme-de-Almeida-Costa <failho42@gmail.com>
2025-06-30 17:08:30 -0300

    att

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,8 +1,27 @@
-exports.index = (req, res) => {
-    if(req.query.searchData) {
-        
+const api = require('../../services/axios');
 
-        res.render("search", { path: "logado", pathStatus: 'LI' });
+exports.index = async (req, res) => {
+    if(req.query.searchData) {
+        const search = req.query.searchData;
+
+        try {
+            const response = await api.post("/search", {
+                search
+            });
+
+            console.log(response.data);
+
+            res.render("search", { path: "logado", pathStatus: 'LI', livros: response.data });
+        } catch (error) {
+            if (error.response) {
+                req.flash('errors', error.response.data.errors);
+            } else {
+                req.flash('errors', ['Erro desconhecido. Verifique a conexão com o servidor.'])
+            }
+            return req.session.save(function(){
+                //arruamar return res.redirect(req.get('/home/search/index'));
+            });
+        }
     } else {
         res.redirect('/home')
     }
